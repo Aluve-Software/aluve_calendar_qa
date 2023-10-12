7afa960967e5eefb630c3e218ac5bf005430ca56
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-12 14:17:41 +0200

    Complete setup

---
+++ src/main/java/org/techtech/aluvesoftware/App.java
@@ -0,0 +1,13 @@
+package org.techtech.aluvesoftware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
